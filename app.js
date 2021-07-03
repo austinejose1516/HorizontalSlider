544229a13348fd24701fc6072abe3008e7547635
Austine Jose <austinejose1516@gmail.com>
2021-07-03 23:00:38 +0100

    completed

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-const images= ['https://unsplash.com/photos/NcdG9mK3PBY', 'https://unsplash.com/photos/fCEJGBzAkrU', 'https://unsplash.com/photos/hgO1wFPXl3I', 'https://unsplash.com/photos/yWF2LLan-_o', 'https://unsplash.com/photos/hzgs56Ze49s', 'https://unsplash.com/photos/MjIMc6uhwrE']
+const divs = document.querySelectorAll('.box');
+const container = document.querySelector('.container')
+
+divs.forEach((img, id) => {
+  img.style.backgroundImage = `url(./images/image${id+1}.jpg)`
+})
+
+window.onscroll = () => {
+  container.style.left = `${-window.scrollY}px`
+}
+
+let current = container.getBoundingClientRect().left;
+
+const callDistort = () => {
+  let newPosition = container.getBoundingClientRect().left
+  let difference = newPosition - current;
+  let speed = difference * 0.35;
+
+  container.style.transform = `skewX(${speed}deg)`;
+  current = newPosition;
+
+  requestAnimationFrame(callDistort)
+}
+callDistort()
+
